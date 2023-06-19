f778c1097d550a7e7691629a95afac5f4a1d0a55
unknown <richardakoekofi@gmail.com>
2023-06-19 09:22:40 +0000

    updated with new features

--- script.js
+++ script.js
@@ -1,40 +1,63 @@
-const inputField = document.getElementById("tasks2create"); 
-const addButton = document.getElementById("add-btn");
-const listGroup = document.getElementById("list-group-class");
+const addBtn = document.getElementById('add-btn');
+const taskInput = document.getElementById('tasks2create');
+const taskList = document.getElementById('list-group-class');
 
-// Add event listeners to the button and list group
-addButton.addEventListener("click", addTask);
-listGroup.addEventListener("click", deleteTask);
+// Add event listener to the Add Task button
+addBtn.addEventListener('click', addTask);
 
+//add a new task
 function addTask() {
-// Get the value from the input field
-const task = inputField.value;
- if (task !== "") {
-  // Create a new list item
-      const listItem = document.createElement("li");
-      listItem.className = "list-group-item";
-      listItem.textContent = task;
-
-      // Create a delete button
-      const deleteButton = document.createElement("button");
-      deleteButton.className = "btn btn-outline-danger btn-sm float-end";
-      deleteButton.textContent = "Delete";
-
-      // Append the delete button to the list item
-      listItem.appendChild(deleteButton);
-
-      // Append the list item to the list group
-      listGroup.appendChild(listItem);
-
-      // Clear the input field
-      inputField.value = "";
-    }
+  const taskText = taskInput.value.trim();
+  if (taskText !== '') {
+    const taskItem = document.createElement('li');
+    taskItem.className = 'list-group-item';
+    taskItem.innerHTML = `
+      <span>${taskText}</span>
+      <div class="float-end">
+        <button class="btn btn-outline-secondary btn-sm" onclick="editTask(this)">Edit</button>
+        <button class="btn btn-outline-danger btn-sm" onclick="deleteTask(this)">Delete</button>
+      </div>
+    `;
+    taskList.appendChild(taskItem);
+    taskInput.value = '';
   }
+}
 
-  function deleteTask(event) {
-    if (event.target.tagName === "BUTTON") {
-      // Remove the parent list item when the delete button is clicked
-      const listItem = event.target.parentNode;
-      listGroup.removeChild(listItem);
-    }
+//delete a task
+function deleteTask(button) {
+  const taskItem = button.closest('li');
+  taskList.removeChild(taskItem);
+}
+
+//edit a task
+function editTask(button) {
+  const taskItem = button.closest('li');
+  const taskText = taskItem.querySelector('span');
+  const editBtn = taskItem.querySelector('.btn-outline-secondary');
+  
+  // Replace the task text 
+  taskText.innerHTML = `<input type="text" class="form-control" value="${taskText.innerText}">`;
+  
+  // Change the "Edit" button to "Save"
+  editBtn.innerText = 'Save';
+  editBtn.classList.replace('btn-outline-secondary', 'btn-outline-primary');
+  editBtn.setAttribute('onclick', 'saveTask(this)');
+}
+
+//save a task after editing
+function saveTask(button) {
+  const taskItem = button.closest('li');
+  const taskText = taskItem.querySelector('span');
+  const editBtn = taskItem.querySelector('.btn-outline-primary');
+  const newTaskText = taskText.querySelector('input').value.trim();
+  
+  // Update the task text if it's not empty
+  if (newTaskText !== '') {
+    taskText.innerText = newTaskText;
   }
+  
+  // Restore the "Edit" button
+  editBtn.innerText = 'Edit';
+  editBtn.classList.replace('btn-outline-primary', 'btn-outline-secondary');
+  editBtn.setAttribute('onclick', 'editTask(this)');
+}
